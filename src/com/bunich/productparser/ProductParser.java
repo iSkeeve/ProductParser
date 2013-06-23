3e3885d7bec3f0f0d422a6e8ed7d00b5ca3726d0
Nick Bunich <iskeeve@yandex.ru>
2013-06-23 23:44:31 +0400

    change xml formatting

--- src/com/bunich/productparser/ProductParser.java
+++ src/com/bunich/productparser/ProductParser.java
@@ -6,7 +6,7 @@ public class ProductParser {
 
     public static void main(String args[]){
         try {
-            createXMLFile(createProductFileList(args), "productss.xml");
+            createXMLFile(createProductFileList(args), "products.xml");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,12 +35,12 @@ public class ProductParser {
     public static File createXMLFile(File files[], String path) throws IOException {
         File xml = new File(path);
         prepareFileForWriting(xml);
-        writeStringToFile("<?xml version=\"1.0\" encoding=\"UTF-8\"?><products>", xml);
+        writeStringToFile(String.format("<?xml version=\"1.0\" encoding=\"UTF-8\"?>%n<products>"), xml);
         /*Делаем запись для каждого продукта из списка files[]*/
         for (File f : files){
             writeProductsToXMLFromFile(f, xml);
         }
-        writeStringToFile("</products>", xml);
+        writeStringToFile(String.format("%n</products>"), xml);
         return xml;
     }
 
@@ -53,8 +53,9 @@ public class ProductParser {
     }
 
     private static void writeProductsToXMLFromFile(File f, File xml) throws IOException {
-        for (String s : createStringFromFile(f).split("\n")){
-            writeStringToFile(createXMLString(s), xml);
+        for (String s : createStringFromFile(f).split(System.getProperty("line.separator"))){
+            /*Сабстринг нужен для удаления разделителя строк*/
+            writeStringToFile(createXMLString(s.substring(0, s.length()-1)), xml);
         }
     }
 
@@ -70,15 +71,15 @@ public class ProductParser {
 
     public static String createXMLString(String string){
         String[] product = string.split(" ");
-        StringBuilder xmlString = new StringBuilder("<product name=\"");
+        StringBuilder xmlString = new StringBuilder(String.format("%n\t<product name=\""));
         xmlString.append(product[0]);
-        xmlString.append("\"><date>");
+        xmlString.append(String.format("\">%n\t\t<date>"));
         xmlString.append(product[3]);
-        xmlString.append("</date><count>");
+        xmlString.append(String.format("</date>%n\t\t<count>"));
         xmlString.append(product[2]);
-        xmlString.append("</count><price>");
+        xmlString.append(String.format("</count>%n\t\t<price>"));
         xmlString.append(product[1]);
-        xmlString.append("</price><product>");
+        xmlString.append(String.format("</price>%n\t</product>"));
         return xmlString.toString();
     }
 
